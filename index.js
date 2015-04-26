504114f89c371642ea8a1cf0a0828aef99dfbfd1
Danny Coates <dannycoates@gmail.com>
2015-04-26 16:19:03 -0700

    added /able/report endpoint

--- index.js
+++ index.js
@@ -22,19 +22,42 @@ module.exports = function (options) {
   }
 
   return function ab(req, res, next) {
+    var pathname = parseUrl(req).pathname
     if (
       options.addRoutes &&
       req.method === 'GET' &&
-      parseUrl(req).pathname === '/experiments.bundle.js'
+      pathname === '/experiments.bundle.js'
     ) {
       getProject(
         function (err, proj) {
           if (err) { return next(err) }
-          var body = Buffer(proj.bundle(), 'utf8')
+          var body = Buffer(proj.bundle('/able/report'), 'utf8')
           res.statusCode = 200
           res.setHeader('Content-Type', 'application/javascript')
           res.setHeader('Content-Length', body.length)
           res.end(body)
+        }
+      )
+      return
+    }
+    else if (
+      options.addRoutes &&
+      options.reportHandler &&
+      req.method === 'POST' &&
+      pathname === '/able/report'
+    ) {
+      options.reportHandler(
+        req.body,
+        function (err) {
+          res.setHeader('Content-Type', 'application/json')
+          if (err) {
+            res.statusCode = 500
+            res.end('{"err":' + JSON.stringify(err.toString()) + '}')
+          }
+          else {
+            res.statusCode = 200
+            res.end('{}')
+          }
         }
       )
       return
